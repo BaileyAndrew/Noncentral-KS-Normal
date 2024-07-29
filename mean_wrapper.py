e0d16cd3620afcf8ca4222746b50dc29d2a55b6f
Bailey Andrew <e.bailey.andrew@gmail.com>
2024-07-29 15:02:20 +0100

    Preliminary iterative tests

--- mean_wrapper.py
+++ mean_wrapper.py
@@ -1,7 +1,8 @@
 import numpy as np
+import scipy.sparse as sparse
 from GmGM import Dataset
 from GmGM.typing import Axis
-from typing import Callable
+from typing import Callable, Literal
 
 class NoncentralKS:
     """
@@ -41,7 +42,18 @@ class NoncentralKS:
         self.full_mean = initial_mean[1]
         self.precision = initial_precision
 
-    def fit(self, data: Dataset) -> tuple[
+    def fit(
+        self,
+        data: Dataset,
+        max_iter: int = 100,
+        tol: float = 1e-4,
+        converge_by: Literal[
+            "means",
+            "objective",
+            "pseudo-objective"
+        ] = "means",
+        verbose: bool = False,
+    ) -> tuple[
         tuple[dict[Axis, np.ndarray], float],
         dict[Axis, np.ndarray]
     ]:
@@ -63,37 +75,126 @@ class NoncentralKS:
             raise ValueError("NoncentralKS only supports one dataset at a time")
         key = list(data.dataset.keys())[0]
 
-        orig_data = data.dataset[key]
+        orig_data = data.dataset[key].copy()
         indices = {
             ell: data.structure[key].index(ell) for ell in data.structure[key]
         }
 
-        # Remove the mean estimate from the data
-        data.dataset = {key: orig_data - self.full_mean}
-        for axis in data.all_axes:
-            data.dataset = {
-                key: data.dataset[key] - self.column_means[axis].reshape(
-                    *(
-                        [1] * indices[axis]
-                        + [-1]
-                        + [1] * (orig_data.ndim - indices[axis] - 1)
+        # TODO: LOOP TILL CONVERGENCE
+
+        converged = False
+        num_iters = 0
+        prev_means = (self.column_means.copy(), self.full_mean)
+        prev_objective = np.inf
+
+        while not converged:
+            # Remove the mean estimate from the data
+            data.dataset = {key: orig_data - self.full_mean}
+            for axis in data.all_axes:
+                data.dataset = {
+                    key: data.dataset[key] - self.column_means[axis].reshape(
+                        *(
+                            [1] * indices[axis]
+                            + [-1]
+                            + [1] * (orig_data.ndim - indices[axis] - 1)
+                        )
                     )
+                }
+
+            # Update precision estimates
+            self.precision = self.estimator(data)
+
+            # Update the mean estimates
+            self.column_means, self.full_mean = mean_estimator(
+                orig_data,
+                self.precision,
+                (self.column_means, self.full_mean),
+                np.prod(orig_data.shape),
+                data.structure[key],
+            )
+
+            # Check if we should stop
+            num_iters += 1
+            if num_iters >= max_iter:
+                if verbose:
+                    print("Maximum number of iterations reached")
+                converged = True
+
+            # Check for convergence
+            if converge_by == "means":
+                squared_dist_1 = sum(
+                    ((prev_means[0][axis] - self.column_means[axis])**2).sum()
+                    for axis in data.structure[key]
                 )
-            }
+                squared_dist_2 = (prev_means[1] - self.full_mean)**2
+                dist = np.sqrt(squared_dist_1 + squared_dist_2)
 
-        # Update precision estimates
-        self.precision = self.estimator(data)
+                if dist < tol:
+                    converged = True
+                    if verbose:
+                        print(f"Converged in {num_iters} iterations")
+            elif converge_by == "objective":
+                # Not practically feasible (involves very large logdet)
+                objective = self.objective(orig_data, data.structure[key])
+                dist = np.abs(prev_objective - objective)
+                prev_objective = objective
 
-        # Update the mean estimates
-        self.column_means, self.full_mean = mean_estimator(
-            orig_data,
-            self.precision,
-            (self.column_means, self.full_mean),
-            np.prod(orig_data.shape),
-            data.structure[key],
-        )
+                if dist < tol:
+                    converged = True
+                    if verbose:
+                        print(f"Converged in {num_iters} iterations")
+            elif converge_by == "pseudo-objective":
+                pseudo_objective = self.pseudo_objective(
+                    orig_data,
+                    data.structure[key]
+                )
+                dist = np.abs(prev_objective - pseudo_objective)
+                prev_objective = pseudo_objective
+
+                if dist < tol:
+                    converged = True
+                    if verbose:
+                        print(f"Converged in {num_iters} iterations")
+
+            if verbose:
+                print(f"Iteration: {num_iters} (Change: {dist})")
+
+            # Update the previous mean estimate
+            prev_means = (self.column_means.copy(), self.full_mean)
+
+        # Reset the data to its original form
+        data.dataset[key] = orig_data
 
         return (self.column_means, self.full_mean), self.precision
+    
+    def objective(self, orig_data: np.ndarray, axes: list) -> float:
+        # Infeasible runtime
+        centralized = orig_data.reshape(-1) - vec_kron_sum([
+            self.column_means[axis]
+            for axis in axes
+        ]) - self.full_mean
+        matrix = kron_sum([
+            self.precision[axis]
+            for axis in axes
+        ])
+        return sparse_logdet(matrix) - centralized @ matrix @ centralized
+    
+    def pseudo_objective(self, orig_data: np.ndarray, axes: list) -> float:
+        centralized = orig_data.reshape(-1) - vec_kron_sum([
+            self.column_means[axis]
+            for axis in axes
+        ]) - self.full_mean
+        matrix = kron_sum([
+            self.precision[axis]
+            for axis in axes
+        ])
+        return centralized @ matrix @ centralized
+    
+def sparse_logdet(matrix: sparse.spmatrix) -> float:
+    """Compute the log-determinant of a sparse matrix"""
+    lu = sparse.linalg.splu(matrix)
+    logdet = np.log(lu.U.diagonal()).sum() + np.log(lu.L.diagonal()).sum()
+    return logdet
     
 def vec_kron_sum(Xs: list) -> np.array:
     """Compute the Kronecker vector-sum"""
@@ -107,6 +208,15 @@ def vec_kron_sum(Xs: list) -> np.array:
             np.kron(Xs[0], np.ones(d_slash0))
             + np.kron(np.ones(Xs[0].shape[0]), vec_kron_sum(Xs[1:]))
         )
+    
+def kron_sum(Xs: list) -> np.array:
+    """Compute the Kronecker sum"""
+    if len(Xs) == 1:
+        return Xs[0]
+    elif len(Xs) == 2:
+        return sparse.kronsum(Xs[0], Xs[1])
+    else:
+        return sparse.kronsum(Xs[0], kron_sum(Xs[1:]))
 
 def mean_estimator(
     data: np.ndarray,
